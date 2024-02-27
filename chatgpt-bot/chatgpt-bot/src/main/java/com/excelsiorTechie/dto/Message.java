86bf1645f2e5704eada9bec2e6936a648d3e2221
Pranav Gujrathi <100834831+PG2575@users.noreply.github.com>
2024-02-27 18:18:53 +0530

    Add files via upload
    STEPS TO INTEGRATE CHAT GPT IN SPRING BOOT APP
    
    Spring IO for App creation with basic SpringWeb and Lombok Dependency
    Create DTO package + Config + Controller packages.
    Basic Functionality and Controller level methods 
    Refer Open AI for Bearer Token creation and save that in resources app.props
    Pass the Get Request in Postman with authentication 
    Add Prompt in Params where we need to pass the Query 
    Check the results of the query.

---
+++ chatgpt-bot/chatgpt-bot/src/main/java/com/excelsiorTechie/dto/Message.java
@@ -0,0 +1,16 @@
+package com.excelsiorTechie.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    public String role;
+
+    public String content;
+
+}
